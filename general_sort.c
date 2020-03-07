d6898da51f13d3ac8c0e064bedc2f173759ac244
Intpro <intprojr@yandex.ru>
2020-03-07 16:19:49 +0300

    general_sort 2

--- general_sort.c
+++ general_sort.c
@@ -6,11 +6,40 @@
 /*   By: lrosalee <lrosalee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 15:33:26 by lrosalee          #+#    #+#             */
-/*   Updated: 2020/03/07 16:08:07 by lrosalee         ###   ########.fr       */
+/*   Updated: 2020/03/07 16:09:58 by lrosalee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** перемещаем значения из структуры a во временный массив intов, зануляем.
+** check_relevance_current: возвращает заполненную структуру actions.
+*/
 
+static void		count_actions(t_stack *a, t_stack *b,
+					t_op *val, t_actions *acts)
+{
+	int		stack_a[500];
+	int		position;
+	int		i;
+
+	i = 0;
+	ft_bzero(stack_a, sizeof(int) * 500);
+	position = 0;
+	acts->a_pos = val->size_a + 1;
+	acts->b_pos = val->size_b + 1;
+	while (a != NULL)
+	{
+		stack_a[i++] = a->value;
+		a = a->next;
+	}
+	while (val->size_b > 0 && b)
+	{
+		check_relevance_current(acts, stack_a, b->value, position++);
+		b = b->next;
+		if (acts->a_pos + acts->b_pos == 1)
+			break ;
+	}
+}
 
 /*
 ** count_actions: возвращает возможные дейтсвия в стеке а или стеке б,
