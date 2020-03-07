ebee363f2d6f2b23bad1d2fc68f49009ab8ced48
Intpro <intprojr@yandex.ru>
2020-03-07 16:10:01 +0300

    general_sort

--- general_sort.c
+++ general_sort.c
@@ -6,9 +6,17 @@
 /*   By: lrosalee <lrosalee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 15:33:26 by lrosalee          #+#    #+#             */
-/*   Updated: 2020/03/07 15:42:40 by lrosalee         ###   ########.fr       */
+/*   Updated: 2020/03/07 16:08:07 by lrosalee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+
+/*
+** count_actions: возвращает возможные дейтсвия в стеке а или стеке б,
+** если перемещения внутри стеков не нужны, то выполняется действие pa.
+** pa: возьмём первый элемент вверху b и поместим его вверх a.
+*/
 
 void 		sorting_inserts(t_stack **a, t_stack **b,
 					t_op *val, int size)
@@ -17,5 +25,24 @@ void 		sorting_inserts(t_stack **a, t_stack **b,
 	int				scrolling;
 
 	acts.val = val;
-	while
+	while ((*b) != NULL)
+	{
+		count_actions(*a, *b, val, &acts);
+		scrolling = acts.a_pos;
+		while (scrolling > 0)
+		{
+			(acts.move_a == 1) ? take_stack_op(a, b, val, "ra")
+				:take_stack_op(a, b, val, "rra");
+			scrolling--;
+		}
+		scrolling = acts.b_pos;
+		while (scrolling > 0)
+		{
+			(acts.move_b == 1) ? take_stack_op(a, b, val, "rb")
+				: take_stack_op(a, b, val, "rrb");
+			scrolling--;
+		}
+		take_stack_op(a, b, val, "pa");
+	}
+	push_min_to_start(a, b, val, size);
 }
