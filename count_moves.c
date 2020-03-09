87db33dba4e0ce89d9584b75811f48f15e8d3a62
Intpro <intprojr@yandex.ru>
2020-03-09 19:12:29 +0300

    count_moves2

--- count_moves.c
+++ count_moves.c
@@ -6,11 +6,13 @@
 /*   By: lrosalee <lrosalee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 18:48:03 by lrosalee          #+#    #+#             */
-/*   Updated: 2020/03/09 18:48:03 by lrosalee         ###   ########.fr       */
+/*   Updated: 2020/03/09 19:10:36 by lrosalee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/push_swap.h"
+
+t_num 	count_stack_a_moves(t_stack)
 
 t_num 	count_moves_for_num(t_stack *a, t_stack *b, t_num val, int i)
 {
@@ -21,6 +23,8 @@ t_num 	count_moves_for_num(t_stack *a, t_stack *b, t_num val, int i)
 	j = a->used_size - 1;
 	half = b->used_size / 2;
 	if (i < half)
+		val.rrb = i + 1;
+	else if (i >= half)
 		val.rb = b->used_size - 1 - i;
 	val = count_stack_a_moves(a, val);
 	val = unit_moves(val);
